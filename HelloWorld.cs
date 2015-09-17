e3a8d05e331369e085a5003ff7ffa51474146a18
jayk-kse3 <jameskiely93@gmail.com>
2015-09-17 13:45:45 +0100

    Create HelloWorld.cs
    Started C# file

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+/* C# Hello world file */
+
+class HelloWorld
+{
+   public static void main(String args[])
+   {
+      string name = "James";
+      Console.WriteLine("\nHello World " + name + "\n");
+   }
+}
